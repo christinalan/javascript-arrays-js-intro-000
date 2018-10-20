9f0d0ece516f20b820c9d8975eb273de1f09d454
Christina Lan <christina.lan13@gmail.com>
2018-10-20 19:30:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
 return array[index]
 }
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {}
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 array.shift(element)
 return array
 }
