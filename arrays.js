3451aaa3f23a577832a5a1cc2c7be8170246dd4c
Christina Lan <christina.lan13@gmail.com>
2018-10-20 19:45:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 array.shift(element)
 return array
 }
-function removeElementFromBeginningOfArray(array, element) {
-  array.slice(2)
+function removeElementFromBeginningOfArray(element, array) {
+  array.slice(1)
   return array
 }
