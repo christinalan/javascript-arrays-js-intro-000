625ca63dd7e866fe7e64cebcda7a4a90c6214bba
Christina Lan <christina.lan13@gmail.com>
2018-10-20 19:23:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,6 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
